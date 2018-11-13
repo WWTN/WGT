76e728ea7f1d199d5555e3afc71e4fc7841a77a5
Ben Johnson <ben.ruby3@gmail.com>
2018-11-13 13:48:29 +0000

    Test
    This is a test

---
+++ ITOSG/Intermediate/Build/Win64/ITOSGEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -0,0 +1,1 @@
+#include "C:/Users/j016593e/Documents/Unreal Projects/ITOSG/Intermediate/Build/Win64/ITOSGEditor/Development/Engine/SharedPCH.Engine.h"
---
+++ ITOSG/Intermediate/Build/Win64/UE4Editor/Inc/ITOSG/ITOSGPlayerController.generated.h
@@ -0,0 +1,88 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef ITOSG_ITOSGPlayerController_generated_h
+#error "ITOSGPlayerController.generated.h already included, missing '#pragma once' in ITOSGPlayerController.h"
+#endif
+#define ITOSG_ITOSGPlayerController_generated_h
+
+#define ITOSG_Source_ITOSG_ITOSGPlayerController_h_12_RPC_WRAPPERS
+#define ITOSG_Source_ITOSG_ITOSGPlayerController_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define ITOSG_Source_ITOSG_ITOSGPlayerController_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAITOSGPlayerController(); \
+	friend struct Z_Construct_UClass_AITOSGPlayerController_Statics; \
+public: \
+	DECLARE_CLASS(AITOSGPlayerController, APlayerController, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/ITOSG"), NO_API) \
+	DECLARE_SERIALIZER(AITOSGPlayerController)
+
+
+#define ITOSG_Source_ITOSG_ITOSGPlayerController_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAITOSGPlayerController(); \
+	friend struct Z_Construct_UClass_AITOSGPlayerController_Statics; \
+public: \
+	DECLARE_CLASS(AITOSGPlayerController, APlayerController, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/ITOSG"), NO_API) \
+	DECLARE_SERIALIZER(AITOSGPlayerController)
+
+
+#define ITOSG_Source_ITOSG_ITOSGPlayerController_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AITOSGPlayerController(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AITOSGPlayerController) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AITOSGPlayerController); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AITOSGPlayerController); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AITOSGPlayerController(AITOSGPlayerController&&); \
+	NO_API AITOSGPlayerController(const AITOSGPlayerController&); \
+public:
+
+
+#define ITOSG_Source_ITOSG_ITOSGPlayerController_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AITOSGPlayerController(AITOSGPlayerController&&); \
+	NO_API AITOSGPlayerController(const AITOSGPlayerController&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AITOSGPlayerController); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AITOSGPlayerController); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AITOSGPlayerController)
+
+
+#define ITOSG_Source_ITOSG_ITOSGPlayerController_h_12_PRIVATE_PROPERTY_OFFSET
+#define ITOSG_Source_ITOSG_ITOSGPlayerController_h_9_PROLOG
+#define ITOSG_Source_ITOSG_ITOSGPlayerController_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	ITOSG_Source_ITOSG_ITOSGPlayerController_h_12_PRIVATE_PROPERTY_OFFSET \
+	ITOSG_Source_ITOSG_ITOSGPlayerController_h_12_RPC_WRAPPERS \
+	ITOSG_Source_ITOSG_ITOSGPlayerController_h_12_INCLASS \
+	ITOSG_Source_ITOSG_ITOSGPlayerController_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define ITOSG_Source_ITOSG_ITOSGPlayerController_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	ITOSG_Source_ITOSG_ITOSGPlayerController_h_12_PRIVATE_PROPERTY_OFFSET \
+	ITOSG_Source_ITOSG_ITOSGPlayerController_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	ITOSG_Source_ITOSG_ITOSGPlayerController_h_12_INCLASS_NO_PURE_DECLS \
+	ITOSG_Source_ITOSG_ITOSGPlayerController_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID ITOSG_Source_ITOSG_ITOSGPlayerController_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
