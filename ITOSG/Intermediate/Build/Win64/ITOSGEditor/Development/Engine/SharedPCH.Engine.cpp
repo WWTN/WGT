c030d9532114d8d348f0016cba64faffb1f687c2
Ben Johnson <ben.ruby3@gmail.com>
2018-11-16 16:50:22 +0000

    Edits

--- ITOSG/Intermediate/Build/Win64/ITOSGEditor/Development/Engine/SharedPCH.Engine.cpp
+++ ITOSG/Intermediate/Build/Win64/ITOSGEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -1,1 +1,1 @@
-#include "C:/Users/j016593e/Documents/GitHub/WGTv2/ITOSG/Intermediate/Build/Win64/ITOSGEditor/Development/Engine/SharedPCH.Engine.h"
+#include "C:/Users/j016593e/Documents/GitHub/WGT/ITOSG/Intermediate/Build/Win64/ITOSGEditor/Development/Engine/SharedPCH.Engine.h"
